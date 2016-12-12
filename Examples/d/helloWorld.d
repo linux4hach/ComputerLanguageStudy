6591fa64f5d68a6ce1c386ce6eefe43fdcf84291
cfair <cfair@hach.com>
2016-12-12 15:39:47 -0700

    Initial commit of computer language study

---
+++ Examples/d/helloWorld.d
@@ -0,0 +1,15 @@
+
+// This works differently than #include
+// in c or c++ but it fundamentally
+// allows you to use code in libraries
+import std.stdio;
+
+// Just like c and c++ d uses the main entry
+int main(string[] args)
+{
+
+  writeln("Hello World"); // Yet another library for writing strings
+  return 0; // return 0
+
+}
+

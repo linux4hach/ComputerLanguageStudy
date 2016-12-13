3d4ba3dcbbce1e03d186c1699429fcd9d5dc5cee
cfair <cfair@hach.com>
2016-12-13 16:13:40 -0700

    added some missing languages

---
+++ Examples/cobol/helloworld.cbl
@@ -0,0 +1,7 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.HELLO_WORLD.
+      *>simple hello world program
+       PROCEDURE DIVISION.
+           Display 'Hello world!'.
+           STOP RUN.
+
